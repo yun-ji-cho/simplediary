3cd466348d0f6f777dc685edb13133059a2b5558
Cho Yunji <favorcho@betree.co.kr>
2022-07-14 11:11:39 +0900

    feat: api 연동

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import "./App.css";
 
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-import Lifecycle from "./Lifecycle";
 
 //https://jsonplaceholder.typicode.com/comments
 
@@ -16,9 +15,24 @@ const App = () => {
   const getData = async () => {
     const res = await fetch(
       "https://jsonplaceholder.typicode.com/comments"
-    ).then((res) => res.json);
-    console.log(res);
+    ).then((res) => res.json());
+
+    const initData = res.slice(0, 20).map((item) => {
+      return {
+        author: item.email,
+        content: item.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_data: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+    setData(initData);
   };
+
+  //App 컴포넌트가 마운트 되자 마자 실행
+  useEffect(() => {
+    getData();
+  }, []);
 
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
@@ -50,7 +64,6 @@ const App = () => {
 
   return (
     <div className="App">
-      <Lifecycle />
       <DiaryEditor onCreate={onCreate} />
       <DiaryList diaryList={data} onDelete={onDelete} onEdit={onEdit} />
       {/* dummyList을 내려주는 것이 아니라 undefined을 내려줘서 에러가 난다면 defaultProps 를 이용한다. 기본값 설정. */}
