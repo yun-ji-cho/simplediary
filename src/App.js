04ac2ce4ce7cc8f041ab5ad46494b18d670bdeff
Cho Yunji <favorcho@betree.co.kr>
2022-07-15 15:36:08 +0900

    feat: useReduce 이용해서 상태변화 로직을 분리하기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
 import "./App.css";
 
-import { useMemo, useState, useRef, useEffect, useCallback } from "react";
+import { useMemo, useReducer, useRef, useEffect, useCallback } from "react";
 
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
 // import OptimizeTest from "./OptimizeTest";
 
+//첫번째 파라미터는 상태변화가 일어나기 직전의 state
+//두번째 파라미터는 어떤 상태 변화가 일어나야 하는지 담은 정보가 담겨있는 action 객체
+//action 객체에 담겨져 있는 type 프로퍼티를 통해서 스위치 문을 이용해서 상태변화 처리를 한다.
+//reducer 가 리턴하는 값이 새로운 상태변화 값이 된다. reducer 는 새로운 state 를 Return 해주는 의무가 있다.
+
+// 즉 dispatch 를 호출하면 reducer 가 실행되고 reducer가 리턴하는 값이 data 의 값이 된다.
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "INIT": {
+      return action.data;
+    }
+    case "CREATE": {
+      const created_data = new Date().getTime();
+      const newItem = {
+        ...action.data,
+        created_data,
+      };
+      return [newItem, ...state];
+    }
+    case "REMOVE": {
+      return state.filter((it) => it.id !== action.targetId);
+    }
+    case "EDIT": {
+      return state.map((it) =>
+        it.id === action.targetId ? { ...it, content: action.newContent } : it
+      );
+    }
+    default:
+      return state;
+  }
+};
+
 const App = () => {
-  const [data, setData] = useState([]);
+  // const [data, setData] = useState([]);
+  const [data, dispatch] = useReducer(reducer, []);
 
   const dataId = useRef(0);
 
@@ -25,35 +58,44 @@ const App = () => {
         id: dataId.current++,
       };
     });
-    setData(initData);
+    dispatch({ type: "INIT", data: initData });
+    // setData(initData);
   };
 
   //App 컴포넌트가 마운트 되자 마자 실행
   useEffect(() => {
     getData();
   }, []);
+
   const onCreate = useCallback((author, content, emotion) => {
-    const created_date = new Date().getTime();
-    const newItem = {
-      author,
-      content,
-      emotion,
-      created_date,
-      id: dataId.current,
-    };
+    dispatch({
+      type: "CREATE",
+      data: { author, content, emotion, id: dataId.current },
+    });
+    // const created_date = new Date().getTime();
+    // const newItem = {
+    //   author,
+    //   content,
+    //   emotion,
+    //   created_date,
+    //   id: dataId.current,
+    // };
     dataId.current += 1;
-    setData((data) => [newItem, ...data]); //setData 에 함수를 전달하는 것을 함수형 업데이트라고 한다. 의존성 배열을 비워도 항상 최신의 state 를 인자를 통해서 참고할수 있게 하여 depth 를 비울 수 있게 한다.
+    // setData((data) => [newItem, ...data]); //setData 에 함수를 전달하는 것을 함수형 업데이트라고 한다. 의존성 배열을 비워도 항상 최신의 state 를 인자를 통해서 참고할수 있게 하여 depth 를 비울 수 있게 한다.
   }, []);
+
   const onRemove = useCallback((targetId) => {
-    setData((data) => data.filter((item) => item.id !== targetId));
+    dispatch({ type: "REMOVE", targetId });
+    // setData((data) => data.filter((item) => item.id !== targetId));
   }, []);
 
   const onEdit = useCallback((targetId, newContent) => {
-    setData((data) =>
-      data.map((it) =>
-        it.id === targetId ? { ...it, content: newContent } : it
-      )
-    );
+    dispatch({ type: "EDIT", targetId, newContent });
+    // setData((data) =>
+    //   data.map((it) =>
+    //     it.id === targetId ? { ...it, content: newContent } : it
+    //   )
+    // );
   }, []);
 
   const getDiaryAnalysis = useMemo(() => {
